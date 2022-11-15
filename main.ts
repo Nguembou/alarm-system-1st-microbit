23baeab0f9fc1c25ff6b1f3b5fc83c459e8317a0
Nguembou <113910948+Nguembou@users.noreply.github.com>
2022-11-15 06:32:28 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
-input.onGesture(Gesture.ThreeG, function () {
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 4) {
+        basic.showIcon(IconNames.Happy)
+        music.stopMelody(MelodyStopOptions.All)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
     radio.sendNumber(2)
+    basic.showLeds(`
+        . # # # .
+        # # . . #
+        # . # . #
+        # . . # #
+        . # # # .
+        `)
+    music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Forever)
     basic.pause(100)
 })
 radio.setGroup(77)
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
 	
 })
