52d700b30010347aa3e25aa9ba09284d15417c9a
Nguembou <113910948+Nguembou@users.noreply.github.com>
2022-11-21 08:27:27 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,7 +13,7 @@ input.onButtonPressed(Button.B, function () {
 radio.setGroup(77)
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-    if (input.acceleration(Dimension.Z) > 432 || input.acceleration(Dimension.Z) < -432) {
+    if (input.acceleration(Dimension.Z) > 612 || input.acceleration(Dimension.Z) < -612) {
         radio.sendNumber(2)
         basic.showLeds(`
             . # # # .
